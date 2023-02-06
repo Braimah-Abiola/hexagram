76c65550115a0acf1ec1358d09cec785e1241ea1
Braimah-Abiola <braimahabiola.official@gmail.com>
2023-02-06 14:20:40 +0000

    Added Watch More

--- src/components/SeeMoreButton.jsx
+++ src/components/SeeMoreButton.jsx
@@ -4,7 +4,7 @@ import styles from '../style'
 
 const SeeMoreButton = () => (
     <>
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-white outline-none rounded-[10px] ${styles}`}>
+    <button onClick={() => window.open('https://www.tiktok.com/@sig.males', '_blank')} type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-white outline-none rounded-[10px] ${styles}`}>
           Watch More
           <img src={arrowUp} className=" w-[13px] h-[13px] object-contain inline-flex ml-5"/>    
     </button>
